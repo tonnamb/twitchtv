4f343852b1de48a360ffc1890513be6341d4caa6
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-07-19 22:20:03 -0400

    working, using forEach and .then from getJSON

--- js/script.js
+++ js/script.js
@@ -6,20 +6,31 @@ $(document).ready(function () {
   "use strict";
 
   (function () {
-    function getTwitch(getWhat, id, callback) {
-      $.getJSON("https://api.twitch.tv/kraken/" + getWhat + "/" + id, function (data) {
-        callback(data);
-      });
+    function getTwitch(getWhat, id) {
+      return $.getJSON("https://api.twitch.tv/kraken/" + getWhat + "/" + id)
+        .then(function (data) {
+          return data;
+        });
     }
     
     var getChannel = getTwitch.bind(null, 'channels');
     var getStream = getTwitch.bind(null, 'streams');
     
+    function checkStatus(id) {
+      return getStream(id).then(function (stream) {
+        if (stream.stream === null) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+
     function divChannel(channel) {
       var $div = $("<div>", {"class": "entry"});
       $div.append("<img class='entry-logo' src='" + channel.logo + "'>");
       $div.append("<p class='entry-name'><a href='https://www.twitch.tv/" + channel.name + "' target='_blank'>" + channel.display_name + "</p>");
-      getStream(channel.name, function (stream) {
+      getStream(channel.name).then(function (stream) {
         if (stream.stream === null) {
           $div.append("<p class='entry-status'>Offline</p>");
         } else {
@@ -29,29 +40,18 @@ $(document).ready(function () {
       return $div;
     }
     
-    function appendToBox(data) {
-      $("#results-box").append(divChannel(data));
+    function appendToBox(channel) {
+      getChannel(channel).then(function (data) {
+        $("#results-box").append(divChannel(data));
+      });
     }
         
     var channelArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     var i;
     
-    for (i = 0; i < channelArray.length; i += 1) {
-      getChannel(channelArray[i], appendToBox);
-    }
+    channelArray.forEach(appendToBox);
     
-    function checkStatus(id) {
-      return $.getJSON("https://api.twitch.tv/kraken/streams/" + id)
-      .then(function(stream) {
-        if (stream.stream === null) {
-          return false;
-        } else {
-          return true;
-        }
-      });
-    }
-    
-    checkStatus(channelArray[0]).then(function(returndata) {
+    checkStatus(channelArray[0]).then(function (returndata) {
       console.log(returndata);
     });
     
