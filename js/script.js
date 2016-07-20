878e08144f14423d495e2040a17aa24c0b5bfe63
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-07-20 00:35:17 -0400

    Got sort by online to work

--- js/script.js
+++ js/script.js
@@ -12,22 +12,12 @@ $(document).ready(function () {
           return data;
         });
     }
-    
+
     var getChannel = getTwitch.bind(null, 'channels');
     var getStream = getTwitch.bind(null, 'streams');
-    
-    function checkStatus(id) {
-      return getStream(id).then(function (stream) {
-        if (stream.stream === null) {
-          return false;
-        } else {
-          return true;
-        }
-      });
-    }
 
     function divChannel(channel) {
-      var $div = $("<div>", {"class": "entry"});
+      var $div = $("<div>", {"class": "entry", "id": channel.name});
       $div.append("<img class='entry-logo' src='" + channel.logo + "'>");
       $div.append("<p class='entry-name'><a href='https://www.twitch.tv/" + channel.name + "' target='_blank'>" + channel.display_name + "</p>");
       getStream(channel.name).then(function (stream) {
@@ -39,22 +29,54 @@ $(document).ready(function () {
       });
       return $div;
     }
-    
+
     function appendToBox(channel) {
       getChannel(channel).then(function (data) {
         $("#results-box").append(divChannel(data));
       });
     }
-        
-    var channelArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+    var channelArray = ["ESL_SC2", "cretetion", "OgamingSC2", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     var i;
-    
+
     channelArray.forEach(appendToBox);
-    
-    checkStatus(channelArray[0]).then(function (returndata) {
-      console.log(returndata);
-    });
-    
+
+    function checkStatus(id) {
+      return getStream(id).then(function (stream) {
+        if (stream.stream === null) {
+          return {name: id.toLowerCase(), status: false};
+        } else {
+          return {name: id.toLowerCase(), status: true};
+        }
+      });
+    }
+
+    function recurCheckStatus(channels) {
+      var iarr = [];
+      function check(arr, index) {
+        if (channels.length === arr.length) {
+          var sortedArr = arr.sort(function (a, b) {return a.status - b.status; });
+          sortDiv(sortedArr);
+        } else {
+          checkStatus(channels[index]).then(function (returndata) {
+            arr.push(returndata);
+            return check(arr, index + 1);
+          });
+        }
+      }
+      return check(iarr, 0);
+    }
+
+    function moveToTop(id) {
+      $("#"+id.name).prependTo("#results-box");
+    }
+
+    function sortDiv(arr) {
+      arr.forEach(moveToTop);
+    }
+
+    recurCheckStatus(channelArray);
+
   }());
 
 });
